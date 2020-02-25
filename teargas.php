b63b96ebbdf5f1297bccdb6d743859352c63da09
Uwezo Niko <uwezo@NBI.KETEPAD.COM>
2020-02-25 18:18:20 +0300

    introduction to html css coding

---
+++ teargas.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tear gas</title>
+	<style>
+img {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+   max-width: 90%;
+  height: auto;
+  align-self:center;
+}
+body{
+	background: grey;
+}
+
+	</style>
+</head>
+<body>
+<h1>
+	A brief description of tear gas
+</h1>
+<ul>
+	<ol>Its origina bane is <em> lachrymator agent or lachrymator</em></ol>
+	<ol>Used mostly in strikes</ol>
+	<ol>Used to disperse rowdy crowds</ol>
+	<ol>Used mostly bb ythe police</ol>
+	<ol>It is irritative when exposed to the eyes</ol>
+</ul>
+<img src="Teargas.jpg">
+</body>
+</html>
